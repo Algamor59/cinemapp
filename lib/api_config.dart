80dcf5c85a89e369e6f9339577ac9b498a85fcc7
JawadBLK <jawad5926@gmail.com>
2024-01-31 17:13:50 +0100

    add api config and feature

---
+++ lib/api_config.dart
@@ -0,0 +1,7 @@
+class ApiConfig {
+  static const String apiKey = '8bb7b190e638ac443678f87a5d0880d0';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+}
+
+  
---
+++ lib/movie_list_view.dart
@@ -0,0 +1,33 @@
+import 'package:cinemapp_app/api_config.dart';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'models/movie.dart';
+
+class MovieListView extends StatelessWidget {
+  final List<Movie> movies;
+
+  const MovieListView({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+        return ListTile(
+          leading: CachedNetworkImage(
+            imageUrl: '${ApiConfig.imageBaseUrl}${movie.posterPath}',
+width: 60,
+            height: 90,
+            fit: BoxFit.cover,
+            placeholder: (context, url) => const CircularProgressIndicator(),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+          title: Text(movie.title),
+subtitle: Text(movie.overview),
+          trailing: Text('Rating: ${movie.voteAverage}'),
+        );
+      },
+    );
+  }
+}
