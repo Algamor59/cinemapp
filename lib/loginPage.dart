4ebeb168ea946bc81f1e0d926746ca7a25def647
Algamor59 <algamor59@gmail.com>
2024-02-07 17:43:46 +0100

    redirection automatique depuis la main vers loginPage

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LoginPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   // This widget is the root of your application.
   @override
