72cb0fc04c8a53f366ad2d57c0cfc2a1b7c9f232
lbarbier22 <lucas.barbier@gastonberger.fr>
2024-02-14 18:34:41 +0100

    feat : add LOGOOOOOOOOOOOOOOO

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cinemapp_app/main.dart';
+import 'package:cinemapp_app/main_api.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
